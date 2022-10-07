a32a682d9982530e31bd39e84fd831e8b6d4b353
Jose Gregorio <115281361+snowflakejgm@users.noreply.github.com>
2022-10-07 13:37:22 -0500

    Replace line by text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit
 
 
-streamlit.title('BreakFast Menu')
-streamlit.title('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.title('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.title(':chicken: Hard-Boiled Free-Range Egg')
-streamlit.title('🥑🍞 Avocado Toast')
+streamlit.header('BreakFast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(':chicken: Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 

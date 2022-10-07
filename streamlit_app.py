25c090900c72640142a871e14331ad0656dba9d5
Jose Gregorio <115281361+snowflakejgm@users.noreply.github.com>
2022-10-07 13:38:33 -0500

    checken emoji  solved

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit
 streamlit.header('BreakFast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text(':chicken: Hard-Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

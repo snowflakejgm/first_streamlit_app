651b989753f4c8f6d2ef2458a79064ad0569afea
Jose Gregorio <115281361+snowflakejgm@users.noreply.github.com>
2022-10-07 13:35:47 -0500

    Add Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,12 @@ import streamlit
 
 
 streamlit.title('BreakFast Menu')
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.title('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.title(':chicken: Hard-Boiled Free-Range Egg')
+streamlit.title('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
 

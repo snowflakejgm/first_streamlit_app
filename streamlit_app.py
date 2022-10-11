d4fc45f77431325ed52186aaf24d1657c3ad1f58
Jose Gregorio <115281361+snowflakejgm@users.noreply.github.com>
2022-10-11 15:48:11 -0500

    Add snowflake connectror

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)

6639ee765dca8d211921f06cd8b95ea642ce2d6f
Jose Gregorio <115281361+snowflakejgm@users.noreply.github.com>
2022-10-11 12:27:56 -0500

    Add textbox for select fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 streamlit.header('BreakFast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -23,6 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
+
 
 
 #streamlit.text(fruityvice_response.json())
